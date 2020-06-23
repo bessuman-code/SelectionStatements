37e57bf552e764863f86096e9406f2ec7d9382bc
U-DESKTOP-1TUJ31R\iisus <comp.structuralbiology@gmail.com>
2020-06-23 12:14:06 -0400

    Selection Statements Practice

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+
+            var favNumb = r.Next(1, 1000);
+            
+            var helpGuess = 1 + favNumb;
+            Console.WriteLine($"Guess a number? {helpGuess} The number should be your number - 1");
+            var userInput = int.Parse(Console.ReadLine());
+            var checkIf = ElseIfState(favNumb, userInput);
+            Console.WriteLine($"Your answer is:");
+            Console.WriteLine($"{userInput} is {checkIf}. It should have been {favNumb}");
+
+            Console.WriteLine("What is your favorite subject?");
+            string input = Console.ReadLine();
+            string callSwitchCase = SwitchCase(input);
+            Console.WriteLine($"{callSwitchCase}");
+
+
+
+            
+        }
+        public static string IfState(int favNumb, int userInput)
+        {
+            
+            if (userInput < favNumb)
+            {
+                return $"Too low";
+            }
+            return $"Whoa";
+
+        }
+        public static string ElseIfState(int favNumb, int userInput)
+        {
+
+            if (userInput < favNumb)
+            {
+                return $"Too low";
+            }
+            else if (userInput > favNumb)
+            {
+                return $"Too big";
+            }
+            else
+            {
+
+
+                return $"Whoa";
+            }
+
+        }
+        public static string ElseState(int favNumb, int userInput)
+        {
+
+            if (userInput < favNumb)
+            {
+                return $"Too low";
+            }
+            else
+            {
+                return $"Never Mind";
+
+            }
+
+        }
+        public static string SwitchCase(String userInput)
+        {
+            var user = userInput;
+            switch (user)
+            {
+                case "Science":
+                    return "Science is awesome";
+                   //Console.WriteLine("Science is awesome!");
+                    //break;
+                case "Math":
+                    return "Math is awesome";
+                    //Console.WriteLine("Math is great!");
+                      //break;
+                default:
+                    return "Any subject is awesome";
+                    //Console.WriteLine("Any subject is great!");
+                    //break;
+
+              }
+            //return null;
+
+        }
+    }  
+
+}
